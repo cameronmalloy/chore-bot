c769aa274dc7d98cfa1305346ae6d80827164198
Cameron Malloy <cmalloy@berkeley.edu>
2019-09-22 08:14:05 -0700

    Changed string

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def create_job(job_name, notif_1, notif_2, senderid):
         print('inserting')
         conn = psycopg2.connect(DATABASE_URL, sslmode='require')
         cur = conn.cursor()
-        cur.execute("INSERT INTO jobs (info) {}".format(str(json.dumps(jobs))))
+        cur.execute("INSERT INTO jobs (info) '{}'".format(json.dumps(jobs)))
         conn.commit()
         cur.close()
         conn.close()
