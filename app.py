1ee7fe51ffc8966251a1ea61b71a2d01327d57bb
Cameron Malloy <cmalloy@berkeley.edu>
2019-09-21 19:34:10 -0700

    Changed log to print

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ def webhook():
     # endpoint for processing incoming messaging events
 
     data = request.get_json()
-    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+    #log(data)  # you may not want to log every incoming message in production, but it's good for testing
+    print(data)
 
     if data["object"] == "page":
 
