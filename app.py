ec5088f6142fe62f6f62aee2437b19375a539554
Cameron Malloy <cmalloy@berkeley.edu>
2019-09-22 13:15:02 -0700

    again... wtf

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+'''
 DATABASE_URL = os.environ['DATABASE_URL']
 DELETE_TABLE = True
-
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
@@ -23,6 +23,7 @@ cur.execute("CREATE TABLE IF NOT EXISTS jobs (job_name varchar, info json);")
 conn.commit()
 cur.close()
 conn.close()
+'''
 
 @app.route('/', methods=['GET'])
 def verify():
@@ -57,11 +58,13 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
+                    '''
                     message_parsed = message_text.split(' ')
                     if message_parsed[0] == '!create':
                         params = message_parsed[1:]
                         params = params + [sender_id]
                         create_job(*params)
+                    '''
                     
                     #send_message(sender_id, "roger that!")
 
@@ -76,6 +79,7 @@ def webhook():
 
     return "ok", 200
 
+'''
 def create_job(job_name, notif_1, notif_2, chores, senderid):
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cur = conn.cursor()
@@ -96,6 +100,7 @@ def create_job(job_name, notif_1, notif_2, chores, senderid):
         send_message(senderid, "That job already exists!")
     cur.close()
     conn.close()
+'''
 
 def send_message(recipient_id, message_text):
 
