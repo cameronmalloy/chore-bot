961ec2852108a590dc9298d2c976a7c95ef84848
Cameron Malloy <cmalloy@berkeley.edu>
2019-09-22 09:31:38 -0700

    Removed db

--- app.py
+++ app.py
@@ -9,15 +9,19 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+'''
 DATABASE_URL = os.environ['DATABASE_URL']
+CREATE_TABLE = True
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
 #cur.execute("DROP TABLE jobs")
-cur.execute("CREATE TABLE IF NOT EXISTS jobs (id serial NOT NULL PRIMARY KEY, job_name varchar, info json);")
+if CREATE_TABLE:
+    cur.execute("CREATE TABLE jobs (id serial NOT NULL PRIMARY KEY, job_name varchar, info json);")
 conn.commit()
 cur.close()
 conn.close()
+'''
 
 with open('jobs.txt') as jobs:
     jobs = json.load(jobs)
@@ -79,6 +83,7 @@ def webhook():
 
     return "ok", 200
 
+'''
 def create_job(job_name, notif_1, notif_2, senderid):
     if not jobs.get(job_name):
         job = {}
@@ -93,6 +98,7 @@ def create_job(job_name, notif_1, notif_2, senderid):
         cur.close()
         conn.close()
     return
+'''
 
 def send_message(recipient_id, message_text):
 
