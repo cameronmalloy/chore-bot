628bb62d89e08269e5d218ec7d737f2a2d66f9ed
Cameron Malloy <cmalloy@berkeley.edu>
2019-09-22 13:02:14 -0700

    removed more chores

--- app.py
+++ app.py
@@ -56,10 +56,8 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    print(message_text)
 
                     message_parsed = message_text.split(' ')
-                    print(chores)
                     if message_parsed[0] == '!create':
                         params = message_parsed[1:]
                         params = params + [sender_id]
