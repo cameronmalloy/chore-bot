451128620b87beedf2dd253bc2d142f4925e732b
Cameron Malloy <cmalloy@berkeley.edu>
2019-09-21 19:28:24 -0700

    Changed nothing

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ app = Flask(__name__)
 def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
-    print('hi')
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
--- env/bin/activate.csh
+++ env/bin/activate.csh
@@ -7,7 +7,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/Users/cameronmalloy/Desktop/projects/fb-messenger-bot/env"
+setenv VIRTUAL_ENV "/Users/cameronmalloy/Desktop/projects/chore-bot/env"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
