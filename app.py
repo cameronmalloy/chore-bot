8fc6184066431f949b67b3e4d3a3826bd88106b1
Cameron Malloy <cmalloy@berkeley.edu>
2019-09-22 09:03:59 -0700

    testing

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cur = conn.cursor()
-cur.execute("DROP TABLE jobs")
-#cur.execute("CREATE TABLE IF NOT EXISTS jobs (id serial NOT NULL PRIMARY KEY, job_name varchar, info json NOT NULL);")
+#cur.execute("DROP TABLE jobs")
+cur.execute("CREATE TABLE IF NOT EXISTS jobs (id serial NOT NULL PRIMARY KEY, job_name varchar, info json NOT NULL);")
 conn.commit()
 cur.close()
 conn.close()
